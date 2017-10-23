7eae6620e897909faaded9701b954694806b488d
cedrvanh <cedrvanh@student.arteveldehs.be>
2017-10-23 17:35:39 +0200

    Added opdrachtweek4

---
+++ opdrachtweek4/fibonacci/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < length; i++){ 
+                Console.WriteLine(Fibonacci(i));
+            }
+
+            int Fibonacci(int n) {
+                int a = 0;
+                int b = 1;
+
+                for (int i = 0; i < n; i++) {
+                    int temp = a;
+                    a = b;
+                    b = temp + b;
+                }
+                return a;
+            }
+        }
+    }
+}
---
+++ opdrachtweek4/priemgetal/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace priemgetal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Geef een getal in die kleiner is dan 100");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if(number > 100) {
+                Console.WriteLine("Geef een getal kleiner dan 100 in.");
+
+            } else {
+                int i;
+                for(i = 0; i <= number; i++) {
+                    Console.WriteLine(i);
+                }
+                if(number % i == 0){
+                    Console.WriteLine("geen priemgetal");
+                } else {
+                    Console.WriteLine("priemgetal");
+                }
+            }
+        }
+    }
+}
